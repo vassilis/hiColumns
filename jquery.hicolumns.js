a6bc1431a57e77500b85c7f672c91e3c6bb30f95
Vassilis Terzopoulos <vassilis@three99.com>
2011-09-01 12:42:38 +0300

    first commit

---
+++ jquery.hicolumns.js
@@ -0,0 +1,26 @@
+(function($){
+	$.fn.hiColumns = function(options){
+		var defaults = {
+			select: 'th',
+			klass: 'highlighted',
+		},
+		options = $.extend({}, defaults, options);
+
+		return this.each(function(){
+			var elem = $(this);
+
+			$(options.select, this).hover(
+				function(){
+					var i = $(this).index();
+
+					elem.find('td').filter(function(){
+						return $(this).index() == i;
+					}).addClass(options.klass);
+				},
+				function(){
+					elem.find('.' + options.klass).removeClass(options.klass);
+				}
+			)
+		})
+	}
+})(jQuery);
